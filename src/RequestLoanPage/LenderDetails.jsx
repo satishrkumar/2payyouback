f2b681429bf62bf1f503aa8c4bea957519dd74e8
Satish Kumar <satish.kumar@hmcts.net>
2021-07-20 08:01:31 +0100

    added loan detail

--- src/RequestLoanPage/LenderDetails.jsx
+++ src/RequestLoanPage/LenderDetails.jsx
@@ -4,6 +4,8 @@ import { requestLoanActions } from "../_actions";
 import { userActions } from "../_actions";
 import "../css/homepage.css";
 import loanRepaymentDetailImg from "../images/loan_repayment_details.png";
+import avatar from "../images/avatar.png";
+import avatar2 from "../images/avatar2.png";
 
 import { render } from "react-dom";
 
@@ -45,8 +47,43 @@ export default function RepaymentDetails() {
             alt="image"
           />
         </div>
-        <div className="col-md-6 marginLeft">Enter lender details</div>
+        <div className="col-md-6 marginLeft lenderdetails">
+        <input
+              type="text"
+              name="loanAmt"
+              placeholder="Enter Email ID/ Mobile Number"
+              className={"form-control"}
+            />
+            <div className="lender">
+                 <div className="row">
+                 <div className="lenderavatar">
+                 <img className="iconClass avatar" src={avatar} />
+                 </div>
+                 <div className="lenderemail">  <label className="labelAlignLeft">raja pidugu<br/>raja@gmail.com</label>
+                 </div>
+                 <div className="lenderbox">
+                 <input type="checkbox" id=""  
+                />
+                 </div>
+               </div>
+      
+               <div className="row">
+                 <div className="lenderavatar">
+                 <img className="iconClass avatar" src={avatar2} />
+                 </div>
+                 <div className="lenderemail">  <label className="labelAlignLeft">nandini valluri<br/>nandinivalluri@gmail.com</label>
+                 </div>
+                 <div className="lenderbox">
+                 <input type="checkbox" id=""  
+                />
+                 </div>
+               </div>
+            
+               </div>
+            
+        </div>
       </div>
     </div>
+    
   );
 }
--- src/RequestLoanPage/PaypalAccount.jsx
+++ src/RequestLoanPage/PaypalAccount.jsx
@@ -22,10 +22,58 @@ export default function LoanDetails() {
     <div className="step4">
       <h5 className="bold">Summary Details</h5>
       <div className="row">
-        <div className="col-md-5">
+        <div className="col-md-4">
           <img className="leftpannelImg" src={loanDetailImg} alt="image" />
         </div>
-        <div className="col-md-5">ffffffffffff</div>
+        <div className="col-md-6 marginLeft">
+        <div className="row">
+        <div className="col-md-12">
+        <div className="form-group">
+            <p className="labelText">Lender's Email ID's</p>
+           <label className="labelAlignLeft">nandini.valluri@purviewservices.com, raja.pidugu@purviewservices.com</label>
+          </div>
+          </div>
+          <div className="col-md-6">
+          <div className="form-group">
+            <p className="labelText">Loan Amount</p>
+            <label className="labelAlignLeft"><span>&euro;</span> 1000</label>
+          </div>
+          </div>
+          <div className="col-md-6">
+          <div className="form-group">
+            <p className="labelText">Offered Interest Rate</p>
+            <label>2% /PM</label>
+          </div>
+          </div>
+          <div className="col-md-6">
+          <div className="form-group">
+            <p className="labelText">Reason for Borrowing</p>
+            <label>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</label>
+          </div>
+        </div>
+       
+        <div className="col-md-6">
+       
+          <div className="form-group">
+            <p className="labelText">Repayment Frequency</p>
+            <label>Monthly</label>
+          </div>
+          </div>
+          <div className="col-md-6">
+          <div className="form-group">
+            <p className="labelText">Amount Payable per month</p>
+            <label><span>&euro;</span> 1000</label>
+          </div>
+          </div>
+          <div className="col-md-6">
+          <div className="form-group">
+            <p className="labelText">Total amount payable by end of term</p>
+            <label><span>&euro;</span> 10000</label>
+          </div>
+        </div>
+        </div>
+        
+        </div>
       </div>
     </div>
   );
