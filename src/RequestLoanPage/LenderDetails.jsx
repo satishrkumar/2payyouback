a5b62db27b482fe48d73403301a336260fa5b8ba
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-09-28 09:23:40 +0100

    Persist lender mail id in LenderDetails.jsx

--- src/RequestLoanPage/LenderDetails.jsx
+++ src/RequestLoanPage/LenderDetails.jsx
@@ -1,53 +1,55 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { requestLoanActions } from "../_actions";
+import React, {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {requestLoanActions} from "../_actions";
 import "../css/homepage.css";
 import loanRepaymentDetailImg from "../images/loan_repayment_details.png";
 
 
-import { ReactMultiEmail, isEmail } from 'react-multi-email';
+import {ReactMultiEmail, isEmail} from 'react-multi-email';
 import 'react-multi-email/style.css';
 
 export default function RepaymentDetails() {
-  const dispatch = useDispatch();
-  const [emails] = useState([])
-  return (
-    <div className="step3 lenderDetails">
-      <h2 className="bold">Lender Details</h2>
-      <div className="row">
-        <div className="col-md-4">
-          <img
-            className="leftpannelImg"
-            src={loanRepaymentDetailImg}
-            alt="image"
-          />
-        </div>
-        <ReactMultiEmail
-            placeholder="Lender Mail Ids"
-            emails={emails}
-            onChange={(_emails) => {
-                //setEmails(_emails)
-                dispatch(requestLoanActions.addLenders(_emails))
-            }}
-            validateEmail={email => {
-              return isEmail(email); // return boolean
-            }}
-            getLabel={(
-                email,
-                index,
-                removeEmail,
-            ) => {
-              return (
-                  <div  key={index}>
-                    {email}
-                    <span onClick={() => removeEmail(index)}>
+    const loanFromState = useSelector((state) => state.loanrequest);
+    const dispatch = useDispatch();
+    const lenders =  loanFromState.lenders != null ? loanFromState.lenders : []
+    const [emails] = useState([...lenders])
+    return (
+        <div className="step3 lenderDetails">
+            <h2 className="bold">Lender Details</h2>
+            <div className="row">
+                <div className="col-md-4">
+                    <img
+                        className="leftpannelImg"
+                        src={loanRepaymentDetailImg}
+                        alt="image"
+                    />
+                </div>
+                <ReactMultiEmail
+                    placeholder="Lender Mail Ids"
+                    emails={emails}
+                    onChange={(_emails) => {
+                        //setEmails(_emails)
+                        dispatch(requestLoanActions.addLenders(_emails))
+                    }}
+                    validateEmail={email => {
+                        return isEmail(email); // return boolean
+                    }}
+                    getLabel={(
+                        email,
+                        index,
+                        removeEmail,
+                    ) => {
+                        return (
+                            <div key={index}>
+                                {email}
+                                <span onClick={() => removeEmail(index)}>
                   ×
                 </span>
-                  </div>
-              );
-            }}
-        />
-      </div>
-    </div>
-  );
+                            </div>
+                        );
+                    }}
+                />
+            </div>
+        </div>
+    );
 }
