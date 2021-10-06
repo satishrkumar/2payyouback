b248b9a021c426d12e43df96fc07e8da1836da2d
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-10-06 10:30:01 +0100

    Update icons based on status and display Borrower email / name

--- src/ApproveLoan/LoanRequest.jsx
+++ src/ApproveLoan/LoanRequest.jsx
@@ -1,8 +1,9 @@
 import React  from "react";
-import "../css/homepage.css";
-import success from "../images/success.svg";
+import success from "../images/approve a loan-icon.svg";
+import pending from "../images/request a loan-icon.svg";
+import reject from "../images/request a loan-icon.svg";
 
-export default function LoanRequest({borrowerEmailId, principal,interest, repaymentDate,reasonForBorrow }) {
+export default function LoanRequest({borrowerEmailId, principal,apr, repaymentDate,reasonForBorrow, loanState }) {
 
   const cardBg = {
     background: "#F8FBFF",
@@ -13,11 +14,12 @@ export default function LoanRequest({borrowerEmailId, principal,interest, repaym
     borderRight: "ridge",
   };
   return (
-      <div className="card w-80" style={cardBg}>
+      <div className="card w-80 mb-2" style={cardBg}>
         <div className="card-body">
           <div className="row">
             <div className="col-md-3">
-              <p className="card-text bold">{borrowerEmailId}</p>
+              <p className="card-title bold">Borrower Name / Mail ID</p>
+              <p className="card-text">{borrowerEmailId}</p>
             </div>
             <div className="col-md-3">
               <p className="card-title bold">Requested Amount</p>
@@ -25,11 +27,17 @@ export default function LoanRequest({borrowerEmailId, principal,interest, repaym
             </div>
             <div className="col-md-3">
               <p className="card-title bold">Offered Interest Rate</p>
-              <p className="card-text">{interest}%</p>
+              <p className="card-text">{apr}%</p>
             </div>
             <div className="col-md-3">
-              <img className="successImg" src={success} alt="image"/>
-              <p className="card-text bold">Accept</p>
+              {loanState === 'PENDING' ?
+                  <img className="w-25 yellow" src={pending} alt="image"/> :
+                  loanState === "APPROVED" ?
+                      <img className="w-25" src={success} alt="image"/> :
+                      <img className="successImg" src={reject} alt="image"/>
+              }
+
+              <p className="card-text bold">{loanState}</p>
             </div>
           </div>
           <br/>
@@ -45,21 +53,25 @@ export default function LoanRequest({borrowerEmailId, principal,interest, repaym
           </div>
           <hr/>
           <div className="row">
-            <div className="col-md-4" style={borderStyle}>
-              <a href="#" className="btn btn-primary">
+            <div className="col-md-3" style={borderStyle}>
+              <a href="#" className="btn btn-success">
+                Accept
+              </a>
+            </div>
+            <div className="col-md-3" style={borderStyle}>
+              <a href="#" className="btn btn-danger">
                 Reject
               </a>
             </div>
-            <div className="col-md-4" style={borderStyle}>
-              <a href="#" className="btn btn-primary">
+            <div className="col-md-3" style={borderStyle}>
+              <a href="#" className="btn btn-info">
                 View Details
               </a>
             </div>
-            <div className="col-md-4">
+            <div className="col-md-3">
               <a
                   href="#"
                   className="btn btn-primary"
-                  // onClick={step1Renegotiate}
               >
                 Renegotiate
               </a>
